280fc476e1d924fefba076868a3f3c8ae88cc3f2
aricart <alberto@synadia.com>
2023-01-12 11:26:07 -0400

    removed keys from test output

--- main_test.go
+++ main_test.go
@@ -3,13 +3,30 @@ package main
 import (
 	"github.com/google/go-cmdtest"
 	"github.com/stretchr/testify/require"
+	"os/exec"
+	"regexp"
 	"testing"
 )
 
 func TestCli(t *testing.T) {
 	ts, err := cmdtest.Read("scripts")
 	require.NoError(t, err)
-	ts.Commands["nsc"] = cmdtest.Program("/Users/aricart/go/bin/nsc")
-	ts.KeepRootDirs = true
-	ts.Run(t, true)
+	binary := "/Users/aricart/go/bin/nsc"
+	ts.Commands["nsc"] = func(args []string, inputFile string) ([]byte, error) {
+		cmd := exec.Command(binary, args...)
+		out, err := cmd.CombinedOutput()
+		require.NoError(t, err)
+		out = filterNkeys(out)
+		return out, err
+	}
+
+	ts.KeepRootDirs = false
+	ts.Run(t, false)
+}
+
+func filterNkeys(data []byte) []byte {
+	var re = regexp.MustCompile(`"S?[OAU][A-Z0-9]{55,56}"`)
+	data = re.ReplaceAll(data, []byte(`"[HIDDEN KEY]"`))
+
+	return data
 }
